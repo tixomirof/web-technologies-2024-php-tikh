1a8de7e8ae351a3616d5802bbf53ba09477d2141
TI <115467358+tixomirof@users.noreply.github.com>
2024-05-08 20:56:11 +0500

    lesson20 v1.3
    lesson20 complete

--- catalog_builder.php
+++ catalog_builder.php
@@ -20,21 +20,28 @@ function initCreation() {
 function createList($id, $all) {
     $children = findChildren($id, $all);
     if (!is_null($children) && count($children) > 0) {
-        echo '<div class="list-item__items">
-        <div class="list-item list-item_open" data-parent>';
         foreach ($children as $row) {
             if (is_null($row)) continue;
             $catalogID = intval($row["catalogID"]);
             $parentID = intval($row["parentID"]);
             $name = $row["catalogName"];
 
-            createFolder($name);
+            echo '<div class="list-item__items">
+            <div class="list-item list-item_open" data-parent>';
+            // P.S. изначально я думал, что будут только папки, поэтому и файлы, и каталоги хранятся у меня в одной таблице
+            // я решил сделать так, что если у папки нет вложенных элементов, то тогда она файл.
+            // (просто переделывать на две таблицы или добавлять лишние столбцы типа "isfile":"true" лень)
+            if (count(findChildren($catalogID, $all)) === 0) {
+                createFile($name);
+            } else {
+                createFolder($name);
+            }
             if (($key = array_search($row, $all)) !== false) {
                 unset($all[$key]);
             }
             createList($catalogID, $all);
+            echo "</div></div>";
         }
-        echo "</div></div>";
     }
 }
 
@@ -44,6 +51,15 @@ function createFolder($foldername) {
         <img class="list-item__arrow" src="./lesson9/img/chevron-down.png" alt="chevron-down" data-open>
         <img class="list-item__folder" src="./lesson9/img/folder.png" alt="folder">
         <span>$foldername</span>
+    </div>
+    EOT;
+}
+
+function createFile($filename) {
+    echo <<<EOT
+    <div class="list-item__inner">
+        <img class="list-item__file" src="./lesson9/img/file.webp" alt="folder">
+        <span>$filename</span>
     </div>
     EOT;
 }
--- lesson9/script.js
+++ lesson9/script.js
@@ -5,122 +5,30 @@ if (document.readyState === 'loading') {
 }
 
 function init() {
-    const data = {
-        name: 'Каталог товаров',
-        hasChildren: true,
-        items: [
-            {
-                name: 'Мойки',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran1',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT1',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT2',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Ulgran2',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT3',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT4',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            },{
-                name: 'Фильтры',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran3',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT5',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT6',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+    const items = new ListItems(document.getElementById('list-items'));
+    const items_mysql = new ListItems(document.getElementById('list-items-mysql'));
 
 
-    const items = new ListItems(document.getElementById('list-items'), data);
-    const items_mysql = new ListItems(document.getElementById('list-items-mysql'), data);
-
-
-  /*  items.render()*/
     items.init();
     items_mysql.init();
 
-    /*console.log(items.renderTest(data));*/
-
-    function ListItems(el, data) {
+    function ListItems(el) {
         this.el = el;
-        this.data = data;
 
         this.init = function () {
             const parents = this.el.querySelectorAll('[data-parent]')
 
             parents.forEach(parent => {
-                const open = parent.querySelector('[data-open]')
+                const open = parent.querySelector('[data-open]');
 
-                open.addEventListener('click', () => this.toggleItems(parent) )
+                if (open != undefined)
+                    open.addEventListener('click', () => this.toggleItems(parent) );
             })
-        }
-
-        this.render = function () {
-            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
-        }
-
-        this.renderParent = function (data) {
-            //проверка всех элементов на hasChildren
-            //если hasChildren, то запускаем renderParent
-            //если !hasChildren, то запускаем renderChildren
-            //возвращает рендер родительского элемента
-
-        }
-
-        this.renderChildren = function (data) {
-            //вовзращает рендер элемента без вложенности
         }
 
         this.toggleItems = function (parent) {
             parent.classList.toggle('list-item_open')
         }
-
-/*        this.renderTest = function (data) {
-            return `
-            <div class="test">${data.name}</div>
-            `
-        }*/
     }
 
 }
